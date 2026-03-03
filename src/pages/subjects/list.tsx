b72f8333cabdbdd24148c8203b9f27c54a9d1f3f
muhammed-codes <dimeji.asynctechs@gmail.com>
2026-03-03 18:40:34 +0100

    style: Add extensive custom Tailwind components, update color palette variables, and refine base styles in `App.css`.

--- src/pages/subjects/list.tsx
+++ src/pages/subjects/list.tsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import { ListView } from "@/components/refine-ui/views/list-view";
+import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb";
+import { Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { DEPARTMENTS_OPTIONS } from "@/constants";
+import { CreateButton } from "@/components/refine-ui/buttons/create";
 
 const SubjectLists = () => {
-  return <div>list</div>;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedDepartment, setSelectedDepartment] = useState("");
+
+  return (
+    <ListView>
+      <Breadcrumb />
+
+      <h1 className="page-title">Subject</h1>
+
+      <div className="intro-row">
+        <p>Quick accesss to essential metrics and management tools.</p>
+      </div>
+      <div className="actions-row">
+        <div className="search-field">
+          <Search className="search-icon" />
+          <Input
+            type="text"
+            placeholder="Search"
+            className="pl-10 w-full"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </div>
+
+        <div className="flex gap-2 w-full sm:w-auto">
+          <Select
+            value={selectedDepartment}
+            onValueChange={setSelectedDepartment}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Select Department" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="all">All Departments</SelectItem>
+              {DEPARTMENTS_OPTIONS.map((department) => (
+                <SelectItem key={department.value} value={department.value}>
+                  {department.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <CreateButton />
+        </div>
+      </div>
+    </ListView>
+  );
 };
 
 export default SubjectLists;
