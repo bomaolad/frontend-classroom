0af2e92eac451fe6de1ab3022a8de7ca5191a29a
muhammed-codes <dimeji.asynctechs@gmail.com>
2026-03-05 12:45:09 +0100

    refactor: replace simple REST data provider with a custom, partially implemented data provider.

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,5 +1,33 @@
-import { createSimpleRestDataProvider } from "@refinedev/rest/simple-rest";
-import { API_URL } from "./constants";
-export const { dataProvider, kyInstance } = createSimpleRestDataProvider({
-  apiURL: API_URL,
-});
+import {
+  BaseRecord,
+  DataProvider,
+  GetListParams,
+  GetListResponse,
+} from "@refinedev/core";
+
+export const dataProvider: DataProvider = {
+  getList: async <TData extends BaseRecord = BaseRecord>({
+    resource,
+  }: GetListParams): Promise<GetListResponse<TData>> => {
+    if (resource !== "subjects") {
+      return { data: [] as TData[], total: 0 };
+    }
+    return {
+      data: [],
+      total: 0,
+    };
+  },
+  getOne: async () => {
+    throw new Error("Not implemented");
+  },
+  create: async () => {
+    throw new Error("Not implemented");
+  },
+  update: async () => {
+    throw new Error("Not implemented");
+  },
+  deleteOne: async () => {
+    throw new Error("Not implemented");
+  },
+  getApiUrl: () => "",
+};
